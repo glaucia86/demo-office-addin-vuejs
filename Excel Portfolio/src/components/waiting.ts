9e6408b6c48cfee0c1f38bd1e42752b9bc1a7d90
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-11-20 17:50:59 -0200

    Início desenvolvimento do componente 'waiting.vue'

---
+++ Excel Portfolio/src/components/waiting.ts
@@ -0,0 +1,15 @@
+/**
+ * File: src/components/waiting.ts
+ * Description:
+ * Date: 11/20/2018
+ * Author: Glaucia Lemos
+ */
+
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+
+export default class waiting extends Vue {
+  name: 'waiting'
+}
