c01d5e3bf3695c093411d8ee5f4f6b677489df6f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-11-20 17:55:13 -0200

    Alteração na estrutura do projeto.

---
+++ Excel Portfolio/src/components/Waiting/waiting.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class="overlay"></div>
+        <img class="spinner" src="../../assets/spinner.gif" />
+    </div>
+</template>
+
+<script lang="ts" src="./waiting.ts" />
+
+<style></style>
